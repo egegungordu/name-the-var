46cd14570945eb918ed27c1d3016bbbfe3b9e07a
Ege Gungordu <egegungorduu@hotmail.com>
2024-01-18 03:58:10 +0300

    figuring out nextjs

--- src/app/name-the/[language]/page.tsx
+++ src/app/name-the/[language]/page.tsx
@@ -1,9 +1,23 @@
+// export const dynamic = "force-dynamic";
 
-export default function GuessingPage({ params }: { params: { language: string } }) {
+export default function GuessingPage({
+  params,
+}: {
+  params: { language: string };
+}) {
+  const renderedAt = new Date().toISOString();
+
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="text-3xl font-semibold text-neutral-700">Guessing Page</div>
-      <div className="text-3xl font-semibold text-neutral-700">{params.language}</div>
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="text-3xl font-semibold text-neutral-700">
+        Guessing Page
+      </div>
+      <div className="text-3xl font-semibold text-neutral-700">
+        {params.language}
+      </div>
+      <div className="text-3xl font-semibold text-neutral-700">
+        {renderedAt}
+      </div>
     </div>
   );
 }
